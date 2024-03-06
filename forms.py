62ac8171ae959adc3ad8734f48d41ece51b8b7d0
andres-ordonezs <andresmw.89@gmail.com>
2024-03-06 14:27:00 -0500

    first commit

---
+++ forms.py
@@ -0,0 +1,55 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import InputRequired, Email, Length, URL, Optional
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[InputRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField(
+        'Username',
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+    email = StringField(
+        'E-mail',
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[InputRequired(), Length(min=6, max=50)],
+    )
+
+    image_url = StringField(
+        '(Optional) Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField(
+        'Username',
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[InputRequired(), Length(min=6, max=50)],
+    )
+
+
+class CsrfForm(FlaskForm):
+    """For actions where we want CSRF protection, but don't need any fields.
+
+    Currently used for our "delete" buttons, which make POST requests, and the
+    logout button, which makes POST requests.
+    """
