a606a8a205a13d335d682504fbe53163bf6e7740
Samuel Myung <shmyung4@gmail.com>
2024-03-08 12:00:55 -0800

    Finished tests for user model

--- test_user_model.py
+++ test_user_model.py
@@ -7,6 +7,8 @@
 
 import os
 from unittest import TestCase
+from sqlalchemy.exc import IntegrityError
+
 
 from models import db, User, Message, Follow
 
@@ -49,3 +51,56 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_is_following_true(self):
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u1.following.append(u2)
+
+        self.assertEqual(u1.is_following(u2), True)
+
+    def test_is_following_false(self):
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        self.assertEqual(u1.is_following(u2), False)
+
+    def test_is_followed_by_true(self):
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u1.followers.append(u2)
+
+        self.assertEqual(u1.is_followed_by(u2), True)
+
+    def test_is_followed_by_false(self):
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        self.assertEqual(u1.is_followed_by(u2), False)
+
+    def test_user_signup_valid(self):
+        u3 = User.signup("u3", "u3@email.com", "password", None)
+
+        self.assertIsInstance(u3, User)
+
+    def test_user_signup_invalid(self):
+        with self.assertRaises(IntegrityError):
+            User.signup("u1", "u3@email.com", "password", None)
+            db.session.commit()
+
+    def test_authenticate_valid(self):
+        result = User.authenticate("u1", "password")
+
+        self.assertEqual(result.id, self.u1_id)
+
+    def test_authenticate_username_invalid(self):
+        result = User.authenticate("u3", "password")
+
+        self.assertEqual(result, False)
+
+    def test_authenticate_password_invalid(self):
+        result = User.authenticate("u1", "pass")
+
+        self.assertEqual(result, False)
